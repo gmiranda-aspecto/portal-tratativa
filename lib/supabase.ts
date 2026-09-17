66f4709b19c42490f2fea1d917b16219b50d39aa
gmiranda-aspecto <gmiranda@aspectoambiental.com.br>
2026-09-17 10:59:02 -0300

    fix: fallback direto na conexao do supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = 
+  process.env.NEXT_PUBLIC_SUPABASE_URL || 
+  'https://qqgskbnqycyncrevyqub.supabase.co';
+
+const supabaseAnonKey = 
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFxZ3NrYm5xeWN5bmNyZXZ5cXViIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4OTU2OTE1MiwiZXhwIjoyMTA1MTQ1MTUyfQ.R_rvtMJjxo31saQcMFWXT0CKdgOdmMHhF77RsT5BBIc';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
